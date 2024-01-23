4eea917587a5462d9ffe27ce8c93214925390cce
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-01-23 23:08:38 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+function initPage() {
+
+}
+
+function validateLogin() {
+
+}
+
+function validateRegistration() {
+
+}
+
+function initContent() {
+    
+}
+
+function logOut() {
+
+}
